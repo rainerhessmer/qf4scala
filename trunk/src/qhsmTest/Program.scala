34203325ab73ceee49e594332e1ddadba9f1e7e5
rainer@hessmer.org <rainer@hessmer.org@61ba8e7c-0d0f-e8db-eee3-f34af30414d4>
2013-03-12 01:40:28 +0000

    Renamed OHsm to QHsm.scala. Fixed ExitUpToLCA function. Tested with the QHsmTest.scala program that implements the hierarchical state machine that Samek uses in his book as an introduction. See StateDiagram.png. The linked pdf document provides a good introduction to QHsm as defined by Samek.

--- trunk/src/qhsmTest/Program.scala
+++ trunk/src/qhsmTest/Program.scala
@@ -5,10 +5,15 @@ object Program {
 	def main(args: Array[String]): Unit  = {
 		val stateMachine = new QHsmTest()
 		stateMachine.InitHsm()
-		print("\nSignal<-")
 		
-		var ok = true
-		while (ok) {
+		object ReadState extends Enumeration {
+			val Consumed, Skipped, End = Value
+		}
+		
+		var readState = ReadState.Consumed
+		while (readState != ReadState.End) {
+			if (readState == ReadState.Consumed) { print("\nSignal<-") }
+			readState = ReadState.Consumed
 			Console.in.read.toChar match {
 				case 'a' => stateMachine.Dispatch(A())
 				case 'b' => stateMachine.Dispatch(B())
@@ -18,8 +23,11 @@ object Program {
 				case 'f' => stateMachine.Dispatch(F())
 				case 'g' => stateMachine.Dispatch(G())
 				case 'h' => stateMachine.Dispatch(H())
-				case _ => ok = false
+				case 'x' => readState = ReadState.End
+				case _ => readState = ReadState.Skipped
 			}
 		}
+		
+		println("\nDone")
 	}
 }
